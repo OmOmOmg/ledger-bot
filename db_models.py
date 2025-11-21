c1731de1aea2acb05a7ca26dbdb80eb2ab2750e5
OmOmOmg <maltsev.nikolay@gmail.com>
2025-11-21 12:57:19 +0700

    remove commented out imports for clarity

--- db_models.py
+++ db_models.py
@@ -1,7 +1,3 @@
-# from __future__ import annotations
-# import os
-# from contextlib import contextmanager
-
 from sqlmodel import SQLModel, Field
 from sqlalchemy import CheckConstraint, UniqueConstraint
 
