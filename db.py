d1fc46e19b24a3947e2eaffaf7ef49a812be2d79
OmOmOmg <maltsev.nikolay@gmail.com>
2025-12-03 12:59:04 +0700

    ver 0.5

--- db.py
+++ db.py
@@ -1,5 +1,4 @@
 import os
-import db
 from sqlmodel import SQLModel, create_engine, Session, Field, select
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -19,22 +18,30 @@ class PoolEntry(SQLModel, table=True):
     kind: str = Field(nullable=False) # "paid" or "share"
     amount_din: int = Field(nullable=False)
 
-def record_debt(bot, message, id: int, username: str, kind: str, amount_din: int, ):
-    with db.get_session() as session: # ensure users exist
-        entry = PoolEntry(
-            id=id,
-            username=username,
-            kind=kind,
-            amount_din=amount_din
-        )
 
-        session.add(entry)
-        session.commit()
+def record_debt(username: str, kind: str, amount_din: int) -> bool:
+    try:
+        with get_session() as session:
+            entry = PoolEntry(
+                username=username,
+                kind=kind,
+                amount_din=amount_din,
+            )
+            session.add(entry)
+            session.commit()
+
+        return True
+
+    except Exception:
+
+        return False
+
+
 
 def my_balance(username: str):
-    with db.get_session() as session:
+    with get_session() as session:
         entries = session.exec(
-            select(db.PoolEntry).where(db.PoolEntry.username == username)
+            select(PoolEntry).where(PoolEntry.username == username)
         ).all()
 
     net = 0
