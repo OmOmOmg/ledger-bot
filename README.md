c9d91ae58a23e2acaa2e4463eb5f0a8867a87950
OmOmOmg <maltsev.nikolay@gmail.com>
2026-01-08 17:20:23 +0100

    ready to add to the group

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 NEXT:
--  add to the group
--  remove usage unstructions
--  change ux to @bot_name command
+x ready to add to the group
+x remove usage instructions 
+x change ux to /paid /share command
+x remove asterix signs from group balance
+
+NEXT:
+
+- depricate paid command
--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def home():
 #curl -F "url=https://nckkk.pythonanywhere.com/" https://api.telegram.org/bot<token>/setWebhook
 
 #process "/start" command
-@bot.message_handler(commands=['start'])
-def start(m):
-    bot.reply_to (m, "Input in format <paid | share> <amount> e.g. paid 1500 \n")
+# @bot.message_handler(commands=['start'])
+# def start(m):
+#     bot.reply_to (m, "Input in format <paid | share> <amount> e.g. paid 1500 \n")
 
 
 @bot.message_handler(commands=["me"])
@@ -46,28 +46,37 @@ def me_handler(message):
 
 @bot.message_handler(commands=["all"])
 def all_handler(message):
-
     lines = []
     for user in db.all_usernames():
         net = db.my_balance(username=user)
         sign = "+" if net > 0 else ""
         lines.append(f"@{user}: {sign}{net} RSD")
 
-    reply_text = "<b>📊 *Group balance:</b>*\n" + "\n".join(lines)
+    reply_text = "<b>📊 Group balance:</b>\n" + "\n".join(lines)
     bot.reply_to(message, reply_text, parse_mode="HTML")
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 #receive imput from user
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    # Expect format: "@username amount"
+    if message.chat.type == "private":
+        return
+
+    if not message.text.startswith("/"):
+        return
+
+    if message.text.startswith(("/me", "/all", "/start")):
+        return
+
     try:
-        kind, amount_str = message.text.split()
+        kind, amount_str = message.text[1:].split()
 
         username = message.from_user.username   # sender of message
         kind = kind.lower()
+
+        if kind not in ("paid", "share"):
+            bot.reply_to(message, "Unknown command. Use /paid or /share.")
+            return
+
         amount = int(amount_str)
 
         ok = db.record_debt(username, kind, amount)
@@ -78,5 +87,7 @@ def handle_text(message):
         else:
             bot.reply_to(message, "Not recorded (error).")
     except Exception:
-        bot.reply_to(message, "Invalid format. Use: <paid | share> <amount>")
+        bot.reply_to(message, "Invalid format. Use: /paid <amount> or /share <amount>")
 
+if __name__ == "__main__":
+    app.run(debug=True)
